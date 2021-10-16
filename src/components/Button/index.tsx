bcb6f1a956197698698badf3748a004b32e0a5a7
Vadym Barabanov <introvert.png@gmail.com>
2021-10-16 22:52:54 +0300

    add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC, ButtonHTMLAttributes, ReactElement } from 'react'
+import styles from './styles.module.scss'
+import clsx from 'clsx'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  primary?: boolean
+  disabled?: boolean
+}
+
+export const Button: FC<ButtonProps> = ({
+  primary = false,
+  disabled = false,
+  children,
+  ...props
+}) => {
+  const classnames = clsx(styles.button, {
+    [styles.primary]: primary,
+    [styles.disabled]: disabled,
+  })
+
+  return (
+    <button className={classnames} {...props}>
+      {children}
+    </button>
+  )
+}
