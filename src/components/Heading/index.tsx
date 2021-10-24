e783277da41b3f43657ebe6b6aaa935a66b92d9b
Vadym Barabanov <introvert.png@gmail.com>
2021-10-24 23:06:24 +0300

    add margins to heading component

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react'
 import clsx from 'clsx'
 import styles from './styles.module.scss'
+import { Margins } from 'types/styles'
 
 type HeadingProps = {
   variant: 'h1' | 'h2' | 'h3'
   fontSize?: 'sm' | 'md' | 'lg'
   align?: 'left' | 'center' | 'right'
+  mt?: Margins
+  mr?: Margins
+  mb?: Margins
+  ml?: Margins
 }
 
 export const Heading: FC<HeadingProps> = ({
@@ -13,10 +18,18 @@ export const Heading: FC<HeadingProps> = ({
   fontSize = 'md',
   children,
   align = 'left',
+  mt,
+  mr,
+  mb,
+  ml,
 }) => {
   const classnames = clsx(styles.heading, {
     [styles[`heading__fontSize--${fontSize}`]]: fontSize,
     [styles[`heading__align--${align}`]]: align,
+    [styles[`mt--${mt}`]]: mt,
+    [styles[`mr--${mr}`]]: mr,
+    [styles[`mb--${mb}`]]: mb,
+    [styles[`ml--${ml}`]]: ml,
   })
 
   switch (variant) {
