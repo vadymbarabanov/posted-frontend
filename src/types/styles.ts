62d52ce48be842606917b9a7ff20c843338dcb86
Vadym Barabanov <introvert.png@gmail.com>
2021-11-03 23:38:03 +0200

    format styles types

--- src/types/styles.ts
+++ src/types/styles.ts
@@ -1,19 +1,25 @@
 export type Margins = 'sm' | 'md' | 'lg' | 'full' | 'auto'
+
 export type MarginsSet = {
   mt?: Margins
   mr?: Margins
   mb?: Margins
   ml?: Margins
 }
+
 export type BoxSizes = 'full' | 'auto' | 'screen'
+
 export type Direction = 'row' | 'column' | 'row-reverse' | 'column-reverse'
+
 export type Gap = 'sm' | 'md' | 'lg'
+
 export type JustifyContent =
   | 'flex-start'
   | 'center'
   | 'flex-end'
   | 'space-between'
   | 'space-around'
+
 export type AlignItems =
   | 'flex-start'
   | 'center'
